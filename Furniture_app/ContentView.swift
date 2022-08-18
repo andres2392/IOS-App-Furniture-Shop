abfec2b530d867096bdac6fa640b6a72f2a69c39
Andres <andres.gonzalez2392@gmail.com>
2022-08-18 08:53:51 -0400

    Complete UI/UX Furniture Shop
    Complete UI/UX Furniture Shop

--- Furniture_app/ContentView.swift
+++ Furniture_app/ContentView.swift
@@ -10,22 +10,13 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        ZStack{
-            Color("Bg")
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack(alignment: .leading){
-                AppBarView()
-                
-                TagLineView()
-                    .padding()
-                
-                SearchAndScanView()
-            }
+        VStack {
+            HomeScreen()
         }
-        
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -33,59 +24,3 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct AppBarView: View {
-    var body: some View {
-        HStack{
-            Button(action: {}){
-                Image("menu")
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(10.0)
-            }
-            Spacer()
-            
-            Image("Profile")
-                .resizable()
-                .frame(width: 42, height: 42)
-                .cornerRadius(10.0)
-        }
-        .padding()
-    }
-}
-
-struct TagLineView: View {
-    var body: some View {
-        Text("Find the \nBest ")
-            .font(.custom("PlayfairDisplay-Regular", size: 28))
-            .foregroundColor(Color("Primary"))
-        + Text("Furniture!")
-            .font(.custom("PlayfairDisplay-Bold", size: 28))
-            .foregroundColor(Color("Primary"))
-    }
-}
-
-struct SearchAndScanView: View {
-    @State private var search: String = ""
-    var body: some View {
-        HStack {
-            HStack{
-                Image("Search")
-                    .padding(.trailing, 8 )
-                TextField("Search Furniture", text: $search)
-            }
-            .padding(.all, 20)
-            .background(Color.white)
-            .cornerRadius(10.0)
-            .padding(.trailing)
-            
-            Button(action: {}){
-                Image("Scan")
-                    .padding()
-                    .background(Color("Primary"))
-                    .cornerRadius(10.0)
-            }
-            
-        }
-        .padding(.horizontal)
-    }
-}
